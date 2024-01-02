15b9c68789f4933bfee5e2a9885461bcf0ce2fee
Kai Black <kaibblack03@gmail.com>
2024-01-01 22:33:01 -0800

    Edit to port

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5000; // Check ./client/package.json "proxy"
+const port = process.env.PORT || 5000; // Check ./client/package.json "proxy"
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
